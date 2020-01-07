a7368733f1f218560ba2673f52c0e27eec68a9fa
joejoe2 <39475537+joejoe2@users.noreply.github.com>
2020-01-08 07:35:10 +0800

    fill materials

---
+++ pic18finalprpject.X/build/default/debug/main.p1.d
@@ -0,0 +1,9 @@
+build/default/debug/main.p1:  \
+main.c  \
+setting_hardaware/setting.h  \
+setting_hardaware/adc.h  \
+setting_hardaware/interrupt_manager.h  \
+setting_hardaware/pin_manager.h  \
+setting_hardaware/LCD.h  \
+setting_hardaware/buzzer.h  \
+setting_hardaware/dht.h 
